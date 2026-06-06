a07102d8a3cd66940c84c37ffa907dbb6ce12bb1
habibur-rahman-tanvir <habibur353637@gmail.com>
2026-06-06 09:33:34 +0600

    Home page design completed

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -25,7 +25,7 @@ const HomePage = () => {
           selectedUser={selectedUser}
           setSelectedUser={setSelectedUser}
         />
-        <RightSidebar />
+        <RightSidebar selectedUser={selectedUser} />
       </div>
     </div>
   );
